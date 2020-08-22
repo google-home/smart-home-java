bed9efb343e86fb91edc46376bd5406c68a02e08
Martynas Mickevičius <self@2m.lt>
2020-08-22 16:55:11 +0300

    Fix resource path
    I was getting the following error in the logs before this fix:
    
    ```
    "errorCode":"You must pass credentials in the app constructor","status":"ERROR"
    ```
    
    Also, this fix was originally mentioned in
    https://github.com/actions-on-google/smart-home-java/issues/10#issuecomment-639776777

--- src/main/java/com/example/SmartHomeServlet.java
+++ src/main/java/com/example/SmartHomeServlet.java
@@ -48,7 +48,7 @@ public class SmartHomeServlet extends HttpServlet {
   {
     try {
       GoogleCredentials credentials =
-          GoogleCredentials.fromStream(getClass().getResourceAsStream("smart-home-key.json"));
+          GoogleCredentials.fromStream(getClass().getResourceAsStream("/smart-home-key.json"));
       actionsApp.setCredentials(credentials);
     } catch (Exception e) {
       LOG.error("couldn't load credentials");
